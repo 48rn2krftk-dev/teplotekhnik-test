2e842bae67b876c1fa4bc2391a313370aa166f09
Drunken Spark Dev <48rn2krftk@privaterelay.appleid.com>
2026-06-20 09:32:40 +1000

    chore: bump test PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "teplotekhnik-test-";
-const CACHE_NAME = `${CACHE_PREFIX}v3`;
+const CACHE_NAME = `${CACHE_PREFIX}v4`;
 const APP_SHELL = new URL("./", self.registration.scope).href;
 const STATIC_FILES = [
   "manifest.webmanifest",
